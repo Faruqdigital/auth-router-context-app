561a1e3b8c5c6c1cee1d6a42bd535e0f08d81cee
Your Name <you@example.com>
2022-11-26 23:56:25 +0600

    context added

--- src/contexts/userContext.js
+++ src/contexts/userContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useEffect, useState } from 'react';
 import {createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut} from 'firebase/auth';
-import app from '../firebase/firebase.config';
+import app from '../firabase/firebase.config';
 
 export const AuthContext = createContext();
 
